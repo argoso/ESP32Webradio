1cbd3d08efbb50d0f997897d6be79cfc03a247f9
sooru <sooru@quretec.com>
2024-11-25 10:46:17 +0200

    update code

--- ESP32webradio/ESP32webradio.ino
+++ ESP32webradio/ESP32webradio.ino
@@ -33,108 +33,68 @@
 #define LINE1_Y 10 // Esimese rea Y-koordinaat
 #define LINE2_Y 20 // Teise rea Y-koordinaat
 
-const char* stations[] = {
-    "https://ice.leviracloud.eu/starFMEesti96-aac",
-    "https://c4.radioboss.fm:18123/stream.m3u",
-    "http://icecast.err.ee/vikerraadio.mp3",
-    "https://cdn.treraadio.ee/ringfm",
-    "https://ice.leviracloud.eu/star96-aac",
-    "https://stream.skymedia.ee/live/SKY",
-    "https://stream.skymedia.ee/live/SKYPLUS",
-    "https://edge02.cdn.bitflip.ee:8888/rck",
-    "https://stream.relaxfm.ee/relax",
-    "https://stream.relaxfm.ee/cafe",
-    "https://router.euddn.net/8103046e16b71d15d692b57c187875c7/kuku_high.mp3",
-    "https://edge06.cdn.bitflip.ee:8888/kadi",
-    "https://router.euddn.net/8103046e16b71d15d692b57c187875c7/elmar.aac",
-    "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/radioduo.aac",
-    "http://icecast.err.ee/raadio2.mp3",
-    "http://icecast.err.ee/r2rock.mp3",
-    "http://icecast.err.ee/r2pop.mp3",
-    "https://icecast.err.ee/r2p.mp3",
-    "http://icecast.err.ee/r2chill.mp3",
-    "http://icecast.err.ee/r2alternatiiv.mp3",
-    "http://icecast.err.ee/r2eesti.mp3",
-    "https://ice.leviracloud.eu/phr96-aac",
-    "https://edge03.cdn.bitflip.ee:8888/NRJ",
-    "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/myhits.aac",
-    "http://5.39.16.62:8004/tre",
-    "http://icecast.err.ee/klassikaraadio.mp3",
-    "http://icecast.err.ee/klaranostalgia.mp3",
-    "http://icecast.err.ee/klaraklassika.mp3",
-    "http://icecast.err.ee/klarajazz.mp3",
-    "https://stream.skymedia.ee/live/KissPride",
-    "https://stream.skymedia.ee/live/KissEurovisioon",
-    "https://stream.skymedia.ee/live/KISS",
-    "https://locomanoco.vlevelscdn.net/radio/kaguraadio.stream/playlist.m3u8",
-    "https://stream.hitmix.ee:8443/HIT_320",
-    "http://s5.radio.co/s69e02764f/listen",
-    "http://www.happyu.ee/streaming/happyu.m3u",
-    "http://icecast.err.ee/raadio2.mp3",
-    "http://icecast.err.ee/klassikaraadio.mp3",
-    "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/elmaritantsuohtu.aac",
-    "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/rokk.aac",
-    "https://router.euddn.net/8103046e16b71d15d692b57c187875c7/duodance.aac",
-    "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/dc_duocountry.aac",
-    "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/dfm.mp3",
-    "https://www.aripaev.ee/raadio/stream.mp3"
+struct Station {
+    const char* name;
+    const char* url;
 };
-const char* stationNames[] = {
-    "00: Star FM Eesti",
-    "01: Võmba FM",
-    "02: Vikerraadio",
-    "03: Tre Raadio Ring FM",
-    "04: Star FM",
-    "05: Sky-Radio",
-    "06: Sky Plus",
-    "07: Rock FM",
-    "08: Relax FM Eesti",
-    "09: Relax Cafe",
-    "10: Raadio Kuku",
-    "11: Raadio Kadi",
-    "12: Raadio Elmar",
-    "13: Raadio Duo",
-    "14: Raadio 2 HQ",
-    "15: R2Rock",
-    "16: R2Pop",
-    "17: R2p",
-    "18: R2Chill",
-    "19: R2Altpop",
-    "20: R2 Eesti",
-    "21: Power Hit Radio",
-    "22: NRJ Tallinn",
-    "23: MyHits",
-    "24: Kuma FM 101.0",
-    "25: Klassikaraadio",
-    "26: Klara Nostalgia",
-    "27: Klara Klassika",
-    "28: Klara Jazz",
-    "29: Kiss FM Pride",
-    "30: Kiss FM Eurovision",
-    "31: Kiss FM",
-    "32: Kaguraadio",
-    "33: Hitmix Estonia",
-    "34: Hard FM Estonia",
-    "35: Happy U",
-    "36: ERR Raadio 2",
-    "37: ERR Klassikaraadio",
-    "38: Elmari tantsuõhtu",
-    "39: Duo Rock",
-    "40: Duo Party",
-    "41: Duo Country",
-    "42: DFM",
-    "43: Äripäeva Raadio"
+
+Station stations[] = {
+    {"00: Star FM Eesti", "https://ice.leviracloud.eu/starFMEesti96-aac"},
+    {"01: Võmba FM", "https://c4.radioboss.fm:18123/stream.m3u"},
+    {"02: Vikerraadio", "http://icecast.err.ee/vikerraadio.mp3"},
+    {"03: Tre Raadio Ring FM", "https://cdn.treraadio.ee/ringfm"},
+    {"04: Star FM", "https://ice.leviracloud.eu/star96-aac"},
+    {"05: Sky-Radio", "https://stream.skymedia.ee/live/SKY"},
+    {"06: Sky Plus", "https://stream.skymedia.ee/live/SKYPLUS"},
+    {"07: Rock FM", "https://edge02.cdn.bitflip.ee:8888/rck"},
+    {"08: Relax FM Eesti", "https://stream.relaxfm.ee/relax"},
+    {"09: Relax Cafe", "https://stream.relaxfm.ee/cafe"},
+    {"10: Raadio Kuku", "https://router.euddn.net/8103046e16b71d15d692b57c187875c7/kuku_high.mp3"},
+    {"11: Raadio Kadi", "https://edge06.cdn.bitflip.ee:8888/kadi"},
+    {"12: Raadio Elmar", "https://router.euddn.net/8103046e16b71d15d692b57c187875c7/elmar.aac"},
+    {"13: Raadio Duo", "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/radioduo.aac"},
+    {"14: Raadio 2 HQ", "http://icecast.err.ee/raadio2.mp3"},
+    {"15: R2Rock", "http://icecast.err.ee/r2rock.mp3"},
+    {"16: R2Pop", "http://icecast.err.ee/r2pop.mp3"},
+    {"17: R2p", "https://icecast.err.ee/r2p.mp3"},
+    {"18: R2Chill", "http://icecast.err.ee/r2chill.mp3"},
+    {"19: R2Altpop", "http://icecast.err.ee/r2alternatiiv.mp3"},
+    {"20: R2 Eesti", "http://icecast.err.ee/r2eesti.mp3"},
+    {"21: Power Hit Radio", "https://ice.leviracloud.eu/phr96-aac"},
+    {"22: NRJ Tallinn", "https://edge03.cdn.bitflip.ee:8888/NRJ"},
+    {"23: MyHits", "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/myhits.aac"},
+    {"24: Kuma FM 101.0", "http://5.39.16.62:8004/tre"},
+    {"25: Klassikaraadio", "http://icecast.err.ee/klassikaraadio.mp3"},
+    {"26: Klara Nostalgia", "http://icecast.err.ee/klaranostalgia.mp3"},
+    {"27: Klara Klassika", "http://icecast.err.ee/klaraklassika.mp3"},
+    {"28: Klara Jazz", "http://icecast.err.ee/klarajazz.mp3"},
+    {"29: Kiss FM Pride", "https://stream.skymedia.ee/live/KissPride"},
+    {"30: Kiss FM Eurovision", "https://stream.skymedia.ee/live/KissEurovisioon"},
+    {"31: Kiss FM", "https://stream.skymedia.ee/live/KISS"},
+    {"32: Kaguraadio", "https://locomanoco.vlevelscdn.net/radio/kaguraadio.stream/playlist.m3u8"},
+    {"33: Hitmix Estonia", "https://stream.hitmix.ee:8443/HIT_320"},
+    {"34: Hard FM Estonia", "http://s5.radio.co/s69e02764f/listen"},
+    {"35: Happy U", "http://www.happyu.ee/streaming/happyu.m3u"},
+    {"36: ERR Raadio 2", "http://icecast.err.ee/raadio2.mp3"},
+    {"37: ERR Klassikaraadio", "http://icecast.err.ee/klassikaraadio.mp3"},
+    {"38: Elmari tantsuõhtu", "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/elmaritantsuohtu.aac"},
+    {"39: Duo Rock", "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/rokk.aac"},
+    {"40: Duo Party", "https://router.euddn.net/8103046e16b71d15d692b57c187875c7/duodance.aac"},
+    {"41: Duo Country", "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/dc_duocountry.aac"},
+    {"42: DFM", "http://router.euddn.net/8103046e16b71d15d692b57c187875c7/dfm.mp3"},
+    {"43: Äripäeva Raadio", "https://www.aripaev.ee/raadio/stream.mp3"}
 };
+
 const int NUM_STATIONS = sizeof(stations) / sizeof(stations[0]);
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Audio audio; // for external DAC
-//Audio audio(true, I2S_DAC_CHANNEL_BOTH_EN); // for internal DAC ESP32
+//Audio audio(true, I2S_DAC_CHANNEL_BOTH_EN); // for internal DAC ESP32 (audio on pins 25 and 26)
 AiEsp32RotaryEncoder rotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
 Preferences preferences; // Loon Preferences objekti
 
 int currentStation = 10;
-int currentVolume = 10;  // Algväärtus helitugevusele 10 (0-20 vahemikus)
+int currentVolume = 10;  // Helitugevuse algväärtus 10 (0-20 vahemikus)
 bool volumeMode = false; // False tähendab jaama valimist, true tähendab helitugevuse muutmist
 
 char streamTitle[64] = "";  // Voogesituse pealkirja salvestamiseks
@@ -222,16 +182,22 @@ void loop() {
             currentStation = constrain(rotaryEncoder.readEncoder(), 0, NUM_STATIONS - 1);
             connectToStation(currentStation);
         }
-        savePreferences(); // Salvestan väärtused kohe, kui need muutuvad
+        savePreferences(); // Salvestan väärtused
         updateDisplay();
     }
 
     if (rotaryEncoder.isEncoderButtonClicked()) {
         volumeMode = !volumeMode;
-        rotaryEncoder.setBoundaries(volumeMode ? 0 : 0, volumeMode ? 20 : NUM_STATIONS - 1, false);
-        updateDisplay();  // Kuvatakse uus režiim OLED ekraanil
-    }
+        if (volumeMode) {
+            rotaryEncoder.setBoundaries(0, 20, false); // Helitugevuse piirid
+            rotaryEncoder.setEncoderValue(currentVolume); // Taasta helitugevuse väärtus
+        } else {
+            rotaryEncoder.setBoundaries(0, NUM_STATIONS - 1, false); // Jaamade piirid
+            rotaryEncoder.setEncoderValue(currentStation); // Taasta jaama väärtus
+        }
+    updateDisplay();
 
+}
     audio.loop();
     delay(10);
 }
@@ -251,6 +217,7 @@ void audio_showstreamtitle(const char *info) {
     decodedTitle.toCharArray(streamTitle, sizeof(streamTitle));
     updateDisplay();  // Kuvatakse dekodeeritud voogesituse pealkiri
 }
+
 // Wifi'ga ühendamine protseduur wlanconfig.ino failist
 void connectToWiFi() {
     while (!makeWLAN()) {
@@ -268,7 +235,7 @@ void initializeAudio() {
 
 void connectToStation(int stationIndex) {
     audio.stopSong();
-    audio.connecttohost(stations[stationIndex]);
+    audio.connecttohost(stations[stationIndex].url);
     updateDisplay();
 }
 
@@ -283,7 +250,7 @@ void updateDisplay() {
         display.println(currentVolume);
     } else {
         // display.print("Station: ");
-        String stationName = decodeUTF8(stationNames[currentStation]);
+        String stationName = decodeUTF8(stations[currentStation].name);
         if (stationName.length() > 17) {
             stationName = stationName.substring(0, 17);
         }
